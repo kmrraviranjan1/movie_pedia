b65f7c42ad5416b5aa049c4fe0926b82211561b7
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-09-21 21:56:37 +0530

    header created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { GlobalStyle } from './GlobalStyle';
+import  {GlobalStyle}  from './GlobalStyle';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle/>
+      <Header />
       Start here.
+      <GlobalStyle/>
     </div>
   );
 }
