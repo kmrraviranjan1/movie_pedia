a0536ba8929992764ac292406a25da08b42adb1e
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-09-22 00:28:21 +0530

    popular movies poster showing on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useHomeFetch } from "../hooks/useHomeFetch";
 import HeroImage from "./HeroImage/HeroImage";
 import Grid from "./Grid/Grid";
-
+import Thumb from "./Thumb/Thumb";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 import NoImage from "../images/no_image.jpg";
@@ -10,20 +10,31 @@ import NoImage from "../images/no_image.jpg";
 const Home = () => {
   const { state, loading, error } = useHomeFetch();
   console.log("movies:", state);
-  return <>
-    {
-        state.results[0] ? 
+  return (
+    <>
+      {state.results[0] ? (
         <HeroImage
-        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-        title={state.results[0].original_title}
-        text={state.results[0].overview}
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
         />
-        : null
-    }
-    <Grid header="Popular Movies">
-       { state.results.map(movie=>(<div key={movie.id} >{movie.title}</div> ))}
-    </Grid>
-  </>;
+      ) : null}
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          />
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default Home;
