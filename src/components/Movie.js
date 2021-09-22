95b83a81c353f92a11b5971b58779940952a5868
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-09-22 10:21:22 +0530

    breadcrumb complete

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import {IMAGE_BASE_URL,POSTER_SIZE} from '../config'
+import Grid from './Grid/Grid'
+import Spinner from './Spinner/Spinner'
+import BreadCrumb from './BreadCrumb/BreadCrumb'
+import {useMovieFetch} from '../hooks/useMovieFetch'
 
+import NoImage from '../images/no_image.jpg'
 const Movie = () => {
+    const {movieId}=useParams()
+    const {state:movie,loading,error}=useMovieFetch(movieId)
+    if(loading)return <Spinner/>
+    if(error)return <div>Something went wrong.. </div>
+ 
     return (
-        <div>
-            Movie
-        </div>
+        <>
+            <BreadCrumb movieTitle={movie.original_title}/>
+        </>
     )
 }
 
