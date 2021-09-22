d3d32b3f3a6353e50e073ede0fc55692d4e34549
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-09-22 08:59:05 +0530

    thumbs clickable and routing path created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
 import  {GlobalStyle}  from './GlobalStyle';
 import Header from './components/Header/Header';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
-function App() {
-  return (
-    <div className="App">
+const App=()=>  (
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/:movieId" element={<Movie/>} />
+        <Route path="/*" element={<NotFound/>} />
+      </Routes>
+      
       <GlobalStyle/>
-    </div>
+    </Router>
   );
-}
+
 
 export default App;
--- src/components/Thumb/Thumb.jsx
+++ src/components/Thumb/Thumb.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import { Image } from './Thumb.styles'
-const Thumb = ({image,movieId,clickable}) => {
-    return (
-        <div>
-            <Image src={image} alt="movie-thumb" />
-        </div>
-    )
-}
+import React from "react";
+import { Link } from "react-router-dom";
+import { Image } from "./Thumb.styles";
+const Thumb = ({ image, movieId, clickable }) => {
+  return (
+    <div>
+      {clickable ? (
+        <Link to={`/${movieId}`}>
+          <Image src={image} alt="movie-thumb" />
+        </Link>
+      ) : (
+        <Image src={image} alt="movie-thumb" />
+      )}
+    </div>
+  );
+};
 
-export default Thumb
+export default Thumb;
