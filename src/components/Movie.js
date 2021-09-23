72992288c765f4064b8a214b67f16c74e134243b
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-09-23 08:49:07 +0530

    actors info done

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,27 +1,46 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import {IMAGE_BASE_URL,POSTER_SIZE} from '../config'
-import Grid from './Grid/Grid'
-import Spinner from './Spinner/Spinner'
-import BreadCrumb from './BreadCrumb/BreadCrumb'
-import MovieInfo from './MovieInfo/MovieInfo'
-import MovieInfoBar from './MovieInfoBar/MovieInfoBar'
-import {useMovieFetch} from '../hooks/useMovieFetch'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+import Grid from "./Grid/Grid";
+import Spinner from "./Spinner/Spinner";
+import BreadCrumb from "./BreadCrumb/BreadCrumb";
+import MovieInfo from "./MovieInfo/MovieInfo";
+import MovieInfoBar from "./MovieInfoBar/MovieInfoBar";
+import Actor from "./Actor/Actor";
+import { useMovieFetch } from "../hooks/useMovieFetch";
 
-import NoImage from '../images/no_image.jpg'
+import NoImage from "../images/no_image.jpg";
 const Movie = () => {
-    const {movieId}=useParams()
-    const {state:movie,loading,error}=useMovieFetch(movieId)
-    if(loading)return <Spinner/>
-    if(error)return <div>Something went wrong.. </div>
- 
-    return (
-        <>
-            <BreadCrumb movieTitle={movie.original_title}/>
-            <MovieInfo movie={movie} />
-            <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
-        </>
-    )
-}
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong.. </div>;
 
-export default Movie
+  return (
+    <>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
+      <Grid header="Actors">
+        {movie.actors.map((actor) => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
+    </>
+  );
+};
+
+export default Movie;
